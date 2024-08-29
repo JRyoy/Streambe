fff777f9fff0455769531584296d277e82662875
JRyoy <josbert0708@gmail.com>
2024-08-29 08:41:35 -0300

    tp4

--- Tp4/java.js
+++ Tp4/java.js
@@ -1,1 +1,8 @@
-let h1
+let boton=document.querySelector("button")
+let parrafo = document.querySelector("p")
+
+
+function cambiarTexto(){
+    parrafo.textContent = "¡Alguien hizo click!"
+}
+boton.addEventListener("click", cambiarTexto)
